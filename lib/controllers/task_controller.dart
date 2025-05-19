64b65bd149c842aa1c83cec9fe385fbf5877a5c7
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2025-05-19 15:01:54 +0300

    refactor(task_controller): reorder methods for better readability
    Reordered methods in TaskController to group related functionality together and improve code readability. The `onReady` method was moved to the bottom, and `getTasks` was repositioned for logical flow.

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -3,28 +3,10 @@ import 'package:todo/db/db_helper.dart';
 import 'package:todo/models/task.dart';
 
 class TaskController extends GetxController {
-  @override
-  void onReady() {
-    // TODO: implement onReady
-    super.onReady();
-  }
-
   final RxList<Task> taskList = <Task>[].obs;
 
   Future<int> addTask({required Task task}) {
     return DBHelper.insert(task);
-  }
-
-  getTasks() async {
-    final List<Map<String, dynamic>> tasks = await DBHelper.query();
-
-    taskList.assignAll(
-      tasks
-          .map(
-            (json) => Task.fromJson(json),
-          )
-          .toList(),
-    );
   }
 
   deleteTasks({required Task task}) async {
@@ -32,8 +14,20 @@ class TaskController extends GetxController {
     getTasks();
   }
 
+  getTasks() async {
+    final List<Map<String, dynamic>> tasks = await DBHelper.query();
+
+    taskList.assignAll(tasks.map((json) => Task.fromJson(json)).toList());
+  }
+
   markTaskAsCompleted({required int id}) async {
     await DBHelper.update(id);
     getTasks();
+  }
+
+  @override
+  void onReady() {
+    // TODO: implement onReady
+    super.onReady();
   }
 }
